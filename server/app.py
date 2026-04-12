b2d0d2b9ec892e265c292b3347b279b8a0c5b169
Santhosh <santhoshdupam2313@gmail.com>
2026-04-12 15:29:19 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -15,5 +15,5 @@ def main(host: str = "0.0.0.0", port: int = 7860):
     import uvicorn
     uvicorn.run(app, host=host, port=port)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
